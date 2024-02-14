0bde2fd0062da401edd13e3fc9f37508850a2649
Tal Even-Tov <tal.eventov@britehouse.co.za>
2024-02-14 12:56:32 +0200

    Added Sample Code - Part 1

---
+++ 1 - Guided Fluent Apis in C# - Part 1/FluentApiDemo/Interfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentApiDemo
+{
+    public interface INewAddress
+    {
+        IStreetAddressSelection ForRecipient(string recipient);
+    }
+
+    public interface IStreetAddressSelection
+    {
+        IPostalCodeSelection WithStreetAddress(string addressLine1, string addresssLine2);
+    }
+
+    public interface IPostalCodeSelection
+    {
+        ICitySelection WithPostalCode(string postalCode);
+    }
+
+    public interface ICitySelection
+    {
+        ICountrySelection InCity(string city);
+    }
+
+    public interface ICountrySelection : ICompleteAddress
+    {
+        ICompleteAddress InCountry(string country);
+    }
+
+    public interface ICompleteAddress
+    {
+        AddressLabel Build();
+    }
+}
---
+++ 1 - Guided Fluent Apis in C# - Part 1/FluentApiDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace FluentApiDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = AddressLabelBuilder
+                .NewAddress()
+                .ForRecipient("ItsAMe Mario")
+                .WithStreetAddress("123 Street", "My Suburb")
+                .WithPostalCode("2192")
+                .InCity("Johannesburg")
+                .InCountry("South Africa")
+                .Build();
+
+            Debugger.Break();
+        }
+    }
+}
